647062140d86db03bed70d2d3c5255dd0fde85d7
Samir Patel <samir.patel@wholefoods.com>
2015-11-10 21:07:06 -0600

    In class coding for cart

--- src/Aca/Bundle/ShopBundle/Controller/CartController.php
+++ src/Aca/Bundle/ShopBundle/Controller/CartController.php
@@ -5,6 +5,8 @@ namespace Aca\Bundle\ShopBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 /**
  * Class CartController deals with all things related to the cart
  * @package Aca\Bundle\ShopBundle\Controller
@@ -13,22 +15,31 @@ class CartController extends Controller
 {
     public function showCartAction()
     {
+        $cart = $this->get('cart');
+
+        $cartProducts = $cart->getAllCartProducts();
+
         return $this->render(
-            'AcaShopBundle:Cart:show.all.html.twig'
+            'AcaShopBundle:Cart:show.all.html.twig',
+            array(
+                'cartProducts' => $cartProducts
+            )
         );
     }
 
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function addCartAction(Request $request)
     {
         $cart = $this->get('cart');
 
         $productId = $request->get('product_id');
-        echo '$productId=' . $productId;
-
-        echo '<br/>';
         $quantity = $request->get('quantity');
-        echo '$quantity=' . $quantity;
 
+        $cart->addProduct($productId, $quantity);
 
+        return new RedirectResponse('/cart');
     }
 }
--- src/Aca/Bundle/ShopBundle/Service/CartService.php
+++ src/Aca/Bundle/ShopBundle/Service/CartService.php
@@ -33,38 +33,95 @@ class CartService
      */
     public function addProduct($productId, $quantity)
     {
+        $cartId = $this->getCartId();
 
+        $productPrice = $this->getProductPrice($productId);
+
+        $insertedId = $this->db->insert('aca_cart_product',
+            array(
+                'cart_id' => $cartId,
+                'product_id' => $productId,
+                'qty' => $quantity,
+                'unit_price' => $productPrice
+            )
+        );
+
+        return !empty($insertedId) && is_int($insertedId) ? true : false;
+    }
+
+    /**
+     * Get the price of one product
+     * @param int $productId
+     * @return float
+     */
+    protected function getProductPrice($productId)
+    {
+        $query = 'select price from aca_product where id = "' . $productId . '"';
+
+        $row = $this->db->fetchRow($query);
+
+        return isset($row['price']) ? $row['price'] : null;
     }
 
     /**
      * Create a cart record, and return the Id, if it doesn't exist
      * If it does exist, just return the Id
+     * @throws \Exception
      * @return int
      */
     public function getCartId()
     {
-        //check if user has a cart
         $userId = $this->session->get('user_id');
-
-        $cartId =
-            'select
-                id
-            from
-                aca_cart
-            where
-                id = ' . $userId;
-
-        $data = $this->db->fetchRow($cartId);
-
-        if (empty($data)) {
-
-            $cartId = $this->db->insert('aca_cart', array('user_id' => $userId));
-
-        } else {
-
-            $cartId = $data['id'];
+        if (empty($userId)) {
+            throw new \Exception('You must be logged in');
         }
 
-        return $cartId;
+        $query = '
+        select
+            id
+        from
+            aca_cart
+        where
+            user_id = :userId';
+
+        $row = $this->db->fetchRow($query, array('userId' => $userId));
+
+        // We have a cart record
+        if (isset($row['id']) && !empty($row)) {
+            return $row['id'];
+        }
+
+        $this->db->insert('aca_cart', array('user_id' => $userId));
+
+        return $this->getCartId();
+    }
+
+    /**
+     * Get all products that are in this user's shopping cart
+     * @throws \Exception
+     * @return array|null
+     */
+    public function getAllCartProducts()
+    {
+        $query = '
+        select
+            cp.id,
+            p.name,
+            p.description,
+            p.image,
+            cp.unit_price as price,
+            cp.qty
+        from
+            aca_cart_product as cp
+            inner join aca_product as p on (p.id = cp.product_id)
+            left join aca_cart as c on (c.id = cp.cart_id)
+        where
+            cp.cart_id = :myCartId';
+
+        return $this->db->fetchRowMany($query,
+            array(
+                'myCartId' => $this->getCartId()
+            )
+        );
     }
 }
